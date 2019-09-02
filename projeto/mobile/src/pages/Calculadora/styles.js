72a0b0a68fedcacb9b5c522f8d6f48f7cf016e8f
Matheus Filipe <mfcr031@gmail.com>
2019-09-02 10:44:14 -0300

    adicionando funcionalidades

---
+++ projeto/mobile/src/pages/Calculadora/styles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export default style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+});
--- projeto/mobile/src/pages/Main/index.js
+++ projeto/mobile/src/pages/Main/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Text, Button } from 'react-native';
-
 import Background from '~/components/Background';
+import style from './styles';
 
-export default function Main() {
+export default function Main({ navigation }) {
+  function Muda() {
+    navigation.navigate('Calculadora');
+  }
+
   return (
-    <Background>
+    <Background style={style.container}>
       <Text>Calculadora de IMC</Text>
-      <Button title="Get Start" />
+      <Button title="Get Start" onPress={Muda} />
     </Background>
   );
 }
