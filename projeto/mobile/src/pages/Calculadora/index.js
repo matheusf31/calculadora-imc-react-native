bdede7b34623f73a73fb26144fc454e3b42518dd
Matheus Filipe <mfcr031@gmail.com>
2019-09-02 10:44:14 -0300

    adicionado funcionalidades

--- projeto/mobile/src/pages/Calculadora/index.js
+++ projeto/mobile/src/pages/Calculadora/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
+import Background from '~/components/Background/index';
+import style from './styles';
 
-export default function Calculadora() {
-  return <View />;
+export default function Calculadora({ navigation }) {
+  function Voltar() {
+    navigation.navigate('Main');
+  }
+
+  return (
+    <Background style={style.container}>
+      <Button title="Voltar" onPress={Voltar} />
+      <Text>Vamos Calcular</Text>
+      <TextInput placeholder="Digite o peso" />
+      <TextInput placeholder="Digite a altura" />
+    </Background>
+  );
 }
--- projeto/mobile/src/pages/Main/style.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-
-const style = StyleSheet.create({});
